b3d7b1fd3ccf07f92d336511cf8f4ae74ce8d22b
kana.fukuzumi <kps.pych.0519km@gmail.com>
2019-04-18 12:29:58 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
    flash[:success] = 'Task が正常に投稿されました'
    redirect_to @task
   else
-   flash.now[:danger] = 'Task　が投稿されませんでした'
+   flash.now[:danger] = 'Task が投稿されませんでした'
    render :new
   end
  end
@@ -49,6 +49,6 @@ class TasksController < ApplicationController
  private
  
  def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
  end
 end
