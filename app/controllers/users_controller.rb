90056deff1ae95d77f20fe53b19ed5e674cd114a
kana.fukuzumi <kps.pych.0519km@gmail.com>
2019-04-23 12:45:40 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-   @users = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
@@ -8,8 +8,23 @@ class UsersController < ApplicationController
   end
 
   def new
+   @user = User.new
   end
 
   def create
+   @user = User.new(user_params)
+   
+   if @user.save
+    flash[:seccess] = 'ユーザーを登録しました'
+    redirect_to @user
+   else
+    flash.now[:danger] = 'ユーザーの登録に失敗しました'
+    render :new
+   end
+  end
+  
+  private
+  def user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_074522) do
+ActiveRecord::Schema.define(version: 2019_04_23_091202) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
